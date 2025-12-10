d096168c4a5c2a24a10a20e06050c8861b3f747c
yossamaulanda <yossamaulanda14@gmail.com>
2025-12-11 05:53:16 +0700

    Update provider.js

--- app/provider.js
+++ app/provider.js
@@ -2,14 +2,9 @@
 
 import { SessionProvider } from 'next-auth/react'
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }) {
   return (
-    <SessionProvider 
-      refetchInterval={5 * 60} // Refetch setiap 5 menit
-      refetchOnWindowFocus={true}
-      // Tambahkan basePath untuk memastikan konsistensi dengan NEXTAUTH_URL
-      basePath={typeof window !== 'undefined' ? undefined : process.env.NEXTAUTH_URL ? `${process.env.NEXTAUTH_URL}/api/auth` : undefined}
-    >
+    <SessionProvider>
       {children}
     </SessionProvider>
   )
